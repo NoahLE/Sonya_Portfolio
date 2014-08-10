49abc0099b2dcaff0ffb3205ff8e921865e30319
DigitalMockingbird <noah@digitalmockingbird.com>
2014-08-10 16:27:34 -0700

    Simplified pause hack

--- js/pause-hack.js
+++ js/pause-hack.js
@@ -1,46 +1,6 @@
-$('#myModal1').on('hidden.bs.modal', function() {
+$(this).on('hidden.bs.modal', function() {
     //removes and readds link to close video when modal is closed
-    var $frame = $('iframe#close1');
-    var vidsrc = $frame.attr('src');
-    $frame.attr('src','');
-    $frame.attr('src', vidsrc);
-});
-
-$('#myModal2').on('hidden.bs.modal', function() {
-    //removes and readds link to close video when modal is closed
-    var $frame = $('iframe#close2');
-    var vidsrc = $frame.attr('src');
-    $frame.attr('src','');
-    $frame.attr('src', vidsrc);
-});
-
-$('#myModal3').on('hidden.bs.modal', function() {
-    //removes and readds link to close video when modal is closed
-    var $frame = $('iframe#close3');
-    var vidsrc = $frame.attr('src');
-    $frame.attr('src','');
-    $frame.attr('src', vidsrc);
-});
-
-$('#myModal4').on('hidden.bs.modal', function() {
-    //removes and readds link to close video when modal is closed
-    var $frame = $('iframe#close4');
-    var vidsrc = $frame.attr('src');
-    $frame.attr('src','');
-    $frame.attr('src', vidsrc);
-});
-
-$('#myModal5').on('hidden.bs.modal', function() {
-    //removes and readds link to close video when modal is closed
-    var $frame = $('iframe#close5');
-    var vidsrc = $frame.attr('src');
-    $frame.attr('src','');
-    $frame.attr('src', vidsrc);
-});
-
-$('#myModal6').on('hidden.bs.modal', function() {
-    //removes and readds link to close video when modal is closed
-    var $frame = $('iframe#close6');
+    var $frame = $('iframe');
     var vidsrc = $frame.attr('src');
     $frame.attr('src','');
     $frame.attr('src', vidsrc);
